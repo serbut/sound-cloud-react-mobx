6a33768006a8c818350021bab9a3665ab2bee138
serhii.butenko <s.butenko.mail@gmail.com>
2020-03-22 22:34:29 +0200

    ref: PlayerStore small ref

--- app/stores/player-store.js
+++ app/stores/player-store.js
@@ -1,66 +1,11 @@
-import { action, computed, extendObservable, observable } from 'mobx';
+import { action, extendObservable } from 'mobx';
 import { createTransformer } from 'mobx-utils';
 
-import { getNextHref, loadMore } from '../api';
-import { isPreview } from '../utils';
+import { getNextHref } from '../api';
+import { Queue } from './player-queue';
 
 const TIME_STEP = 15;
 const VOLUME_STEP = 0.25;
-
-const filterData = data => {
-  return data
-    .filter(
-      el =>
-        (Object.prototype.hasOwnProperty.call(el, 'origin') && el.origin) ||
-        true
-    )
-    .filter(el => el.type === 'track' || el.type === 'track-repost')
-    .map(el => el.origin || el);
-};
-
-class Queue {
-  @observable originItems = [];
-  @observable isLoading;
-
-  constructor(player) {
-    this.player = player;
-  }
-
-  @computed get items() {
-    if (this.player.skipPreviews)
-      return this.originItems.filter(el => !isPreview(el));
-    else return this.originItems;
-  }
-
-  @computed get trackIndex() {
-    return this.items.findIndex(i => i.id === this.player.track.id);
-  }
-
-  @computed get prevTrack() {
-    return this.items[this.trackIndex - 1];
-  }
-
-  @computed get nextTrack() {
-    return this.items[this.trackIndex + 1];
-  }
-
-  @computed get randomTrack() {
-    return this.items[Math.floor(Math.random() * this.items.length)];
-  }
-
-  loadMore() {
-    if (this.isLoading || !this.nextHref) return;
-
-    this.isLoading = true;
-    loadMore(this.nextHref).then(
-      action(data => {
-        filterData(data.collection).forEach(el => this.originItems.push(el));
-        this.nextHref = data.next_href;
-        this.isLoading = false;
-      })
-    );
-  }
-}
 
 class PlayerStore {
   constructor() {
