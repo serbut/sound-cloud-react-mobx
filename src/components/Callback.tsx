4b8059a523e0e40982739efb45f7f68c603290a4
serhii.butenko <s.butenko.mail@gmail.com>
2020-04-12 17:20:01 +0300

    feature: Move to Function Components - App, Callback, ScrollToTopBtn

--- src/components/Callback.tsx
+++ src/components/Callback.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export default class Callback extends React.Component {
-  componentDidMount() {
+const Callback = () => {
+  useEffect(() => {
     window.setTimeout(window.opener.SC.connectCallback, 1);
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <p>This page should close soon.</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <p>This page should close soon.</p>
+    </div>
+  );
+};
+
+export default Callback;
