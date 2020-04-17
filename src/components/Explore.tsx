222afec65e1a1ba86174b264d532713fecf67d70
serhii.butenko <s.butenko.mail@gmail.com>
2020-04-17 16:58:44 +0300

    ref: Explore page

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,36 +1,40 @@
-import { Paper, Tab, Tabs } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
-import React, { useContext, useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import qs from 'qs';
+import React, { useContext } from 'react';
+import {
+  Redirect,
+  useHistory,
+  useLocation,
+  useRouteMatch,
+} from 'react-router-dom';
 import { AppContext } from '../app-context';
 import useDataLoader from '../hooks/use-data-loader';
 import { Track } from '../models/track';
 import DataGrid from './DataGrid';
+import { ExploreTabs } from './ExploreTabs';
 
-export const GENRES_MAP: {
-  [key: string]: string;
-} = {
-  ambient: 'Ambient',
-  deephouse: 'Deep House',
-  electronic: 'Electronic',
-  house: 'House',
-  techno: 'Techno',
-  trap: 'Trap',
-  hiphop: 'Hip Hop',
-  dance: 'Dance',
-  pop: 'Pop',
-  'drum&bass': 'Drum & Bass',
+export type ExploreTab = {
+  value: string;
+  label: string;
 };
 
-const GENRES_LIST: string[] = [];
-
-for (const key in GENRES_MAP) {
-  GENRES_LIST.push(key);
-}
+const TABS: ExploreTab[] = [
+  { value: 'ambient', label: 'Ambient' },
+  { value: 'deephouse', label: 'Deep House' },
+  { value: 'electronic', label: 'Electronic' },
+  { value: 'house', label: 'House' },
+  { value: 'techno', label: 'Techno' },
+  { value: 'trap', label: 'Trap' },
+  { value: 'hiphop', label: 'Hip Hop' },
+  { value: 'dance', label: 'Dance' },
+  { value: 'pop', label: 'Pop' },
+  { value: 'drumandbass', label: 'Drum & Bass' },
+];
 
 const Explore = () => {
   const history = useHistory();
   const location = useLocation();
+  const { path } = useRouteMatch();
   const { api } = useContext(AppContext);
   const genre = new URLSearchParams(location.search).get('genre');
   const dataLoaderProps = useDataLoader<Track[]>(api.endpoints.tracks, {
@@ -38,51 +42,37 @@ const Explore = () => {
     tags: genre,
   });
 
-  useEffect(() => {
-    // TODO: somehow improve this maybe?
-    const searchParams = new URLSearchParams();
-    searchParams.append('genre', GENRES_LIST[0]);
-
-    if (!genre) {
-      history.replace({
-        pathname: '/explore',
-        search: searchParams.toString(),
-      });
-    }
-  });
-
   const handleChange = (event: React.ChangeEvent<{}>, index: number) => {
-    const searchParams = new URLSearchParams();
-    searchParams.append('genre', GENRES_LIST[index]);
-
     history.push({
-      pathname: '/explore',
-      search: searchParams.toString(),
+      pathname: path,
+      search: qs.stringify({
+        genre: TABS[index].value,
+      }),
     });
   };
 
   if (!genre) {
-    return null;
+    return (
+      <Redirect
+        to={{
+          pathname: path,
+          search: qs.stringify({
+            genre: TABS[0].value,
+          }),
+        }}
+      />
+    );
   }
 
-  const currentTabIndex = GENRES_LIST.indexOf(genre);
+  const currentTabIndex = TABS.findIndex((tab) => tab.value === genre);
 
   return (
     <div>
-      {currentTabIndex !== -1 && (
-        <Paper square>
-          <Tabs
-            textColor="primary"
-            indicatorColor="primary"
-            value={currentTabIndex}
-            onChange={handleChange}
-          >
-            {GENRES_LIST.map((el: string, i) => (
-              <Tab key={i} label={GENRES_MAP[el]} />
-            ))}
-          </Tabs>
-        </Paper>
-      )}
+      <ExploreTabs
+        tabs={TABS}
+        currentTabIndex={currentTabIndex}
+        handleChange={handleChange}
+      />
 
       <div className="container" style={{ paddingTop: 48 + 48 }}>
         <DataGrid {...dataLoaderProps} />
---
+++ src/components/ExploreTabs.tsx
@@ -0,0 +1,32 @@
+import { Paper, Tab, Tabs } from '@material-ui/core';
+import React from 'react';
+import { ExploreTab } from './Explore';
+
+export const ExploreTabs = ({
+  tabs,
+  currentTabIndex,
+  handleChange,
+}: {
+  tabs: ExploreTab[];
+  currentTabIndex: number;
+  handleChange: (event: React.ChangeEvent<{}>, index: number) => void;
+}) => {
+  if (currentTabIndex === -1) {
+    return null;
+  }
+
+  return (
+    <Paper square>
+      <Tabs
+        textColor="primary"
+        indicatorColor="primary"
+        value={currentTabIndex}
+        onChange={handleChange}
+      >
+        {tabs.map((tab, i) => (
+          <Tab key={i} label={tab.label} />
+        ))}
+      </Tabs>
+    </Paper>
+  );
+};
