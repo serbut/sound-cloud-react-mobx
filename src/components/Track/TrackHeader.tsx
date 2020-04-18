0d85c12f17bb609bd06d13bdafc66dd2a42a909f
serhii.butenko <s.butenko.mail@gmail.com>
2020-04-18 22:04:30 +0300

    fixup! feature: TrackView refactoring & some UI changes

--- src/components/Track/TrackHeader.tsx
+++ src/components/Track/TrackHeader.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Container, Grid, Typography } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Hidden,
+  Typography,
+} from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
@@ -10,43 +17,53 @@ import TrackCard from './TrackCard';
 const TrackHeader = ({ track }: { track: Track }) => {
   const { sessionStore } = useContext(AppContext);
   const { user } = track;
+  const isLiked = sessionStore.isLiked(track);
 
   return (
     <Box py={3} mb={3} style={{ backgroundColor: '#eeeeee99' }}>
       <Container>
-        <Grid container alignItems="center" spacing={2}>
-          <Grid item xs={3}>
-            <TrackCard track={track} compact />
-          </Grid>
+        <Grid container alignItems="center" spacing={3}>
+          <Hidden smDown>
+            <Grid item sm={3}>
+              <TrackCard track={track} compact />
+            </Grid>
+          </Hidden>
 
-          <Grid item xs={9}>
-            <Typography variant="subtitle1">Single</Typography>
-            <Typography variant="h5" gutterBottom>
+          <Grid item xs>
+            <Typography variant="h5">Single</Typography>
+            <Typography variant="h4" gutterBottom>
               {track.title}
             </Typography>
-            <Typography variant="subtitle1">
+            <Typography variant="body1" display="inline">
               by{' '}
-              <Link to={`/users/${user.permalink}`}>
-                <Typography variant="body1" color="primary" display="inline">
-                  {user.username}
-                </Typography>
-              </Link>
             </Typography>
-            <Typography variant="body2" paragraph>
-              added {fromNow(track.created_at)}{' '}
+            <Link to={`/users/${user.permalink}`}>
+              <Typography
+                variant="body1"
+                color="primary"
+                display="inline"
+                gutterBottom
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            <Typography variant="body2" gutterBottom>
+              added {fromNow(track.created_at)}
               <span className="bullet">&bull;</span>
               {formatDuration(track.duration)}
               <span className="bullet">&bull;</span>
-              {formatNumber(track.playback_count)} plays{' '}
+              {formatNumber(track.playback_count)} plays
               <span className="bullet">&bull;</span>
-              {formatNumber(track.reposts_count)} reposts{' '}
+              {formatNumber(track.favoritings_count)} likes
+              {/*<span className="bullet">&bull;</span>*/}
+              {/*{formatNumber(track.reposts_count)} reposts{' '}*/}
             </Typography>
             <Button
-              variant={sessionStore.isLiked(track) ? 'contained' : 'outlined'}
-              color="primary"
+              variant="contained"
+              color={isLiked ? 'default' : 'primary'}
               onClick={() => sessionStore.toggleLike(track)}
             >
-              {formatNumber(track.favoritings_count)} likes
+              {isLiked ? 'Unlike' : 'Like'}
             </Button>
           </Grid>
         </Grid>
