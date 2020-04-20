9e74c6f565079c3ebdcf6c1da61825cf0be27fe6
serhii.butenko <s.butenko.mail@gmail.com>
2020-04-20 15:12:58 +0300

    ref: ScrollToTopBtn upd

--- src/components/ScrollToTopBtn.tsx
+++ src/components/ScrollToTopBtn.tsx
@@ -1,19 +1,33 @@
 import { IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { KeyboardArrowUp } from '@material-ui/icons';
+import debounce from 'debounce';
 import React, { useEffect, useState } from 'react';
-import './ScrollToTopBtn.css';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    position: 'fixed',
+    bottom: 100,
+    right: 64,
+    zIndex: 1000,
+  },
+  button: {
+    border: `1px solid ${theme.palette.secondary.main}`,
+  },
+}));
 
 const ScrollToTopBtn = () => {
   const [isVisible, setVisible] = useState<boolean>(false);
+  const classes = useStyles();
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.pageYOffset > 1000) {
+    const handleScroll = debounce(() => {
+      if (window.pageYOffset > window.innerHeight * 2) {
         setVisible(true);
       } else {
         setVisible(false);
       }
-    };
+    }, 400);
 
     window.addEventListener('scroll', handleScroll);
 
@@ -26,9 +40,13 @@ const ScrollToTopBtn = () => {
 
   if (isVisible)
     return (
-      <div className="ScrollToTopBtn">
-        <IconButton color="primary" onClick={handleScrollToTopClick}>
-          <KeyboardArrowUp />
+      <div className={classes.container}>
+        <IconButton
+          color="secondary"
+          className={classes.button}
+          onClick={handleScrollToTopClick}
+        >
+          <KeyboardArrowUp fontSize="large" />
         </IconButton>
       </div>
     );
