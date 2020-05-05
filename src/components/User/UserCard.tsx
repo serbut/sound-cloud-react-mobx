40f9ce4ea8fc5ba0d754d5645d9d70b53e358b8a
serhii.butenko <s.butenko.mail@gmail.com>
2020-05-05 21:32:58 +0300

    ref: UserCard styles

--- src/components/User/UserCard.tsx
+++ src/components/User/UserCard.tsx
@@ -1,24 +1,44 @@
 import { Avatar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { ImageSize } from '../../enums';
 import { User } from '../../models/user';
 import { formatNumber, getImageUrl } from '../../utils';
-import './UserCard.css';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: '100%',
+    height: 'auto',
+    paddingTop: '100%',
+    boxShadow: `0px 1px 5px 0px rgba(0, 0, 0, 0.2),
+      0px 2px 2px 0px rgba(0, 0, 0, 0.14), 
+      0px 3px 1px -2px rgba(0, 0, 0, 0.12)`,
+    fontSize: 0,
+    '& img': {
+      position: 'absolute',
+      top: 0,
+    },
+  },
+  content: {
+    padding: theme.spacing(2, 2, 4),
+  },
+}));
 
 const UserCard = ({ user }: { user: User }) => {
+  const classes = useStyles();
   const link = `/users/${user.permalink}`;
 
   return (
-    <div className="UserCard">
+    <div>
       <Link to={link}>
         <Avatar
           alt={user.username}
           src={getImageUrl(user.avatar_url, ImageSize.t500x500)}
-          className="UserCard-avatar"
+          className={classes.avatar}
         />
       </Link>
-      <div className="UserCard-content">
+      <div className={classes.content}>
         <Typography variant="subtitle2" align="center">
           <Link to={link}>{user.username}</Link>
         </Typography>
