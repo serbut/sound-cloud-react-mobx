a8851e1363f789ff3579473752d52811e112ddd0
serhii.butenko <s.butenko.mail@gmail.com>
2020-05-09 18:05:41 +0300

    feature: UserLinks component created

--- src/components/User/UserAbout.tsx
+++ src/components/User/UserAbout.tsx
@@ -1,43 +1,18 @@
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
 import { User } from '../../models/user';
-
-// TODO: Create new component
-// const renderLinks = (links: { url: string; title: string }[]) => {
-//   if (!links || links.length === 0) {
-//     return '';
-//   }
-//
-//   return (
-//     <div className="UserAbout">
-//       <Typography variant="h5" gutterBottom>
-//         Links
-//       </Typography>
-//       <ul className="UserAbout-links">
-//         {links.map((el, i) => (
-//           <li key={i}>
-//             <a href={el.url}>
-//               <Typography color="primary">{el.title}</Typography>
-//             </a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+import UserLinks from './UserLInks';
 
 const UserAbout = ({ user }: { user: User }) => {
-  if (!user.description) {
-    return null;
-  }
-
   return (
-    <div className="UserAbout">
-      <Typography variant="h5">Description</Typography>
-      <pre>
-        <Typography variant="body2">{user.description}</Typography>
-      </pre>
-    </div>
+    <Box py={3}>
+      {user.description && (
+        <pre>
+          <Typography variant="body2">{user.description}</Typography>
+        </pre>
+      )}
+      <UserLinks user={user} />
+    </Box>
   );
 };
 
