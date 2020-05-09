8bddb1bbf6be06ddbf19e490f9bd16d49785fc91
serhii.butenko <s.butenko.mail@gmail.com>
2020-05-09 16:46:41 +0300

    ref: TrackCard, TrackImage correlation

--- src/components/Track/TrackCard.tsx
+++ src/components/Track/TrackCard.tsx
@@ -1,9 +1,13 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
-import React from 'react';
+import { Card, CardContent, IconButton, Typography } from '@material-ui/core';
+import { Pause, PlayArrow } from '@material-ui/icons';
+import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AppContext } from '../../app-context';
 import { Track } from '../../models/track';
 import { formatNumber, fromNow } from '../../utils';
 import { Bullet } from '../Bullet';
+import Overlay from '../Overlay';
 import TrackImage from './TrackImage';
 
 const TrackCard = ({
@@ -11,11 +15,36 @@ const TrackCard = ({
   tracks, // TODO: TrackCard should nщt know about other tracks
 }: {
   track: Track;
-  tracks?: Track[];
+  tracks: Track[];
 }) => {
+  const { playerStore } = useContext(AppContext);
+
+  const handlePlayClick = () => {
+    playerStore.playTrack(track, tracks.slice());
+  };
+
+  const isCurrentTrack = !!playerStore.isSelected(track);
+  const isPlaying = playerStore.isSelected(track) === 'isPlaying';
   return (
     <Card>
-      <TrackImage track={track} tracks={tracks} />
+      <Overlay
+        overlayContent={
+          <IconButton
+            onClick={handlePlayClick}
+            aria-label={isPlaying ? 'pause' : 'play'}
+          >
+            {isPlaying ? (
+              <Pause fontSize="large" color="secondary" />
+            ) : (
+              <PlayArrow fontSize="large" color="secondary" />
+            )}
+          </IconButton>
+        }
+        shown={isCurrentTrack}
+        showOnMouseIn={true}
+      >
+        <TrackImage track={track} />
+      </Overlay>
       <CardContent>
         <Typography variant="subtitle2" noWrap>
           <Link to={`/users/${track.user.permalink}/tracks/${track.permalink}`}>
@@ -37,4 +66,4 @@ const TrackCard = ({
   );
 };
 
-export default TrackCard;
+export default observer(TrackCard);
